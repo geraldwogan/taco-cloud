58dca9f1f7170ef70ee361aa394b3269d5b6dd1d
geraldwogan <geraldwogan@gmail.com>
2023-01-22 15:05:19 +0000

    feat: added not null and size validation for Taco names

--- src/main/java/geraldwogan/tacocloud/model/Taco.java
+++ src/main/java/geraldwogan/tacocloud/model/Taco.java
@@ -3,9 +3,14 @@ package geraldwogan.tacocloud.model;
 import java.util.List;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class Taco {
 
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
 
     private List<Ingredient> ingredients;
