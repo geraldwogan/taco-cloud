3b6a9765382c277de24d37d6e038d4282fcda785
geraldwogan <geraldwogan@gmail.com>
2023-01-23 23:16:51 +0000

    feat: add validation at form binding

--- src/main/java/geraldwogan/tacocloud/controller/OrderController.java
+++ src/main/java/geraldwogan/tacocloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package geraldwogan.tacocloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import geraldwogan.tacocloud.model.TacoOrder;
 import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -22,7 +25,12 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus){
+    public String processOrder(@Valid TacoOrder order,
+                               Errors errors,
+                               SessionStatus sessionStatus){
+        if(errors.hasErrors()){
+            return "orderForm";
+        }
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
 
