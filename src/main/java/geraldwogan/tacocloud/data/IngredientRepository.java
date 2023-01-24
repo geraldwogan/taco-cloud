79adc4eee13760c62b6fcd9c45e451e1e6a8ff07
geraldwogan <geraldwogan@gmail.com>
2023-01-24 01:18:40 +0000

    feat: adding Ingredient Repository Interface

---
+++ src/main/java/geraldwogan/tacocloud/data/IngredientRepository.java
@@ -0,0 +1,14 @@
+package geraldwogan.tacocloud.data;
+
+import geraldwogan.tacocloud.model.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+
+    Optional<Ingredient> findById(String id);
+
+    Ingredient save(Ingredient ingredient);
+}
