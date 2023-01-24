1c84e7b4486c745234e8343de0ded2727ae694ee
geraldwogan <geraldwogan@gmail.com>
2023-01-24 00:52:51 +0000

    feat: adapting model classes for persistence

--- src/main/java/geraldwogan/tacocloud/model/Taco.java
+++ src/main/java/geraldwogan/tacocloud/model/Taco.java
@@ -1,5 +1,6 @@
 package geraldwogan.tacocloud.model;
 
+import java.util.Date;
 import java.util.List;
 import lombok.Data;
 
@@ -8,6 +9,10 @@ import javax.validation.constraints.Size;
 
 @Data
 public class Taco {
+
+    private Long id;
+
+    private Date createdAt = new Date();
 
     @NotNull
     @Size(min=5, message="Name must be at least 5 characters long")
